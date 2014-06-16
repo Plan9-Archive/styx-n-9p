c5c71603764d167a13ea3aff7c277dd8235a0f26
Charles Forsyth <charles.forsyth@gmail.com>
2014-06-16 10:02:08 +0100

    javadoc; enquiry functions; String.format instead of specialised functions

--- java/src/plan9/Qid.java
+++ java/src/plan9/Qid.java
@@ -1,33 +1,81 @@
 package plan9;
 
+// Qid, from styx-n-9p.googlecode.com (MIT Licence)
+
+/**
+ * Each file is the responsibility of some server: it could be
+ * a file server, a kernel device, or a user process.  Type
+ * identifies the server type, and dev says which of a group of
+ * servers of the same type is the one responsible for this
+ * file.  Qid is a structure containing path and vers fields:
+ * path is guaranteed to be unique among all path names currently on the file server,
+ * and vers changes each time the file is modified.
+ * The path is a 64-bit integer and the vers is an unsigned
+ * 32-bit integer (represented in Java by a long).
+ * Thus, if two files have the same type, dev, and qid they are the same file.
+ */
 public class Qid {
-	public long	path;
-	public int		vers;
+
+	/** 64-bit value, unique to a file on a given file server */
+	public long	path;	
+
+	/** 32-bit unsigned value incremented on each write */
+	public int		vers;	
+
+	/** file's type: the top 8 bits of its Dir.mode */
 	public int		qtype;
 
 	// Qid.qtype type bits
-	public static final int	QTDIR = 0x80;		// directory
-	public static final int	QTAPPEND = 0x40;	// append-only file
-	public static final int	QTEXCL = 0x20;	// exclusive-use file
-	public static final int	QTAUTH = 0x08;	// authentication file
-	public static final int	QTTMP = 0x04;		// not-backed-up file (symbolic link in 9P200.u)
-	public static final int	QTFILE = 0x00;		// plain file
 
+	/** directory */
+	public static final int	QTDIR = 0x80;	
+
+	/** append-only file */
+	public static final int	QTAPPEND = 0x40;
+
+	/** exclusive-use file */
+	public static final int	QTEXCL = 0x20;
+
+	/** authentication file */
+	public static final int	QTAUTH = 0x08;
+
+	/** not-backed-up file (symbolic link in 9P200.u) */
+	public static final int	QTTMP = 0x04;	
+
+	/** plain file */
+	public static final int	QTFILE = 0x00;	
+
+	/** Create a new Qid value with given path and type, and version zero */
 	public Qid(long path, int qtype){
 		this.path = path; this.vers = 0; this.qtype = qtype;
 	}
+
+	/** Create a new Qid value with given path, vers(ion), and type */
 	public Qid(long path, int vers, int qtype){
 		this.path =  path; this.vers = vers; this.qtype = qtype;
 	}
-	public boolean equals(Qid q){
-		return q != null && path == q.path && vers == q.vers && qtype == q.qtype;
+
+	/** Return true iff Object <i>o</i> is a Qid of equal value */
+	public boolean equals(Object o){
+		if(o == null || !(o instanceof Qid))
+			return false;
+		if(o == this)
+			return true;
+		Qid q = (Qid)o;
+		return path == q.path && vers == q.vers && qtype == q.qtype;
 	}
+
+	/** Return a value usable as a Java hash code */
 	public int hashCode(){
-		return (int)path ^ vers;
+		return (int)path ^ vers ^ qtype;
 	}
+
+	/** Return a string with a printable version of the Qid components */
 	public String toString(){
-		return "("+pad(Long.toHexString(path),16)+" "+vers+" "+pad(Integer.toHexString(qtype),2)+")";
+		return String.format("(%016x %d %02x)", path, vers, qtype);
 	}
+
+	/** Return a string with a printable version of the Qid [q]type */
 	public String qidtype(){
 		char[] s = new char[5];
 		int i = 0;
@@ -42,14 +90,5 @@ public class Qid {
 		if((qtype & QTTMP) != 0)
 			s[i++] = 'T';
 		return new String(s, 0, i);
-	}
-	private static final String pad(String s, int n){
-		while(s.length() < n){
-			int j = n-s.length();
-			if(j > 10)
-				j = 10;
-			s = "0000000000".substring(0, j)+s;
-		}
-		return s;
 	}
 }
