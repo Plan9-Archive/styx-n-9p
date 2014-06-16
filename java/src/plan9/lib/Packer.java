bd74e51b77f1ee944171bea7d9ffd90a8c6cd907
Charles Forsyth <charles.forsyth@gmail.com>
2014-06-16 09:26:26 +0100

    move String operations to Strings; add get8, offset

--- java/src/plan9/lib/Packer.java
+++ java/src/plan9/lib/Packer.java
@@ -1,6 +1,6 @@
 package plan9.lib;
 
-// Vita Nuova, MIT Licence
+// from styx-n-9p.googlecode.com, MIT licence
 
 import java.lang.*;
 import java.util.Arrays;
@@ -12,6 +12,10 @@ public class Packer {
 	int	o0;	// initial offset
 	int	o;
 
+	public Packer(int n){
+		o = o0 = 0;
+		data = new byte[n];
+	}
 	public Packer(byte[] a){
 		o = o0 = 0;
 		data = a;
@@ -29,7 +33,16 @@ public class Packer {
 	public ByteBuffer buffer(){
 		return ByteBuffer.wrap(data, o0, length());
 	}
+	public byte[] array(){
+		return data;
+	}
+	public int offset(){
+		return o;
+	}
 
+	public byte get(){
+		return data[o++];
+	}
 	public int get1(){
 		return (int)data[o++] & 0xFF;
 	}
@@ -50,6 +63,10 @@ public class Packer {
 		o += 4;
 		return v;
 	}
+	public long get8(){
+		long n0 = (long)get4() & 0xFFFFFFFFL;
+		return ((long)get4() << 32) | n0;
+	}
 	public byte[] geta(int n){
 		byte[] a = new byte[n];
 		System.arraycopy(data, o, a, 0, n);
@@ -57,15 +74,23 @@ public class Packer {
 		return a;
 	}
 
+	public final void put(byte b){
+		data[o++] = b;
+	}
+	public final void put1(int v){
+		data[o++] = (byte)(v & 0xFF);
+	}
 	public final void put2(int v){
 		data[o+0] = (byte)(v);
 		data[o+1] = (byte)(v>>8);
+		o += 2;
 	}
 	public final void put4(int v){
 		data[o+0] = (byte)v;
 		data[o+1] = (byte)(v>>8);
 		data[o+2] = (byte)(v>>16);
 		data[o+3] = (byte)(v>>24);
+		o += 4;
 	}
 	public final void put8(long v){
 		data[o+0] = (byte)v;
@@ -76,64 +101,54 @@ public class Packer {
 		data[o+5] = (byte)(v>>40);
 		data[o+6] = (byte)(v>>48);
 		data[o+7] = (byte)(v>>56);
+		o += 8;
 	}
 
-	public void puta(byte[] a, int n){
+	public final void puta(byte[] a, int n){
 		if(a != null && a.length != 0){
 			int l = a.length < n? a.length: n;
 			System.arraycopy(a, 0, data, o, l);
-			if(l < n)
-				n -= l;
+			o += l;
+			n -= l;
 		}
-		if(n > 0)
-			Arrays.fill(data, o, o+n-1, (byte)0);
+		if(n > 0){
+			Arrays.fill(data, o, o+n, (byte)0);
+			o += n;
+		}
+	}
+	public final void puta(byte[] a){	// a must not be null
+		puta(a, a.length);
 	}
 
-	public String gets(int n){	// fixed-length string with null byte
+	public final String gets(int n){	// fixed-length string with null byte
 		try{
+			int o1 = o;
+			o += n;
 			if(n > 0)
 				n--;	// must have null byte
 			for(int i = 0; i < n; i++){
-				if(data[i] == (byte)0){
+				if(data[o1+i] == (byte)0){
 					n = i;
 					break;
 				}
 			}
-			return new String(data, o, n, "UTF-8");
+			return new String(data, o1, n, "UTF-8");
 		}catch(java.io.UnsupportedEncodingException e){
 			return "Egosling";
 		}
 	}
 
-	public void puts(String s){	// string with explicit count
+	public final void puts(String s){	// string with explicit count
 		if(s == null || s.length() == 0){
 			put2(0);
 			return;
 		}
-		byte[] a = bytes(s);
+		byte[] a = Strings.bytes(s);
 		put2(a.length);
 		puta(a, a.length);
 	}
 
-	private final void puta(String s, int n) {	// string as fixed array
-		puta(bytes(s), n);
-	}
-
-	private static final String S(byte[] b){
-		try{
-			return new String(b, "UTF-8");
-		}catch(java.io.UnsupportedEncodingException e){
-			return "Egosling";
-		}
-	}
-
-	public static final byte[] bytes(String s){
-		if(s == null)
-			return new byte[0];
-		try{
-			return s.getBytes("UTF-8");
-		}catch(java.io.UnsupportedEncodingException e){
-			return "Egosling".getBytes();
-		}
+	public final void puts(String s, int n) {	// string as fixed array
+		puta(Strings.bytes(s), n);
 	}
 }
