0799896f24d715e5097183d4493217c1bc7560e7
Charles Forsyth <charles.forsyth@gmail.com>
2014-06-16 08:57:41 +0100

    rename encoding to base64

--- java/src/test/D64.java
+++ java/src/test/D64.java
@@ -1,9 +1,10 @@
-import com.vitanuova.lib.Base64;
+import plan9.lib.Base64;
 
+// test base64 decoding
 public class D64 {
 	public static void main(String[] args){
-		Base64 encoding = new Base64();
-		byte[] b = encoding.dec(args[0]);
+		Base64 base64 = new Base64();
+		byte[] b = base64.dec(args[0]);
 		System.out.println(b.length+": "+new String(b));
 	}
 }
--- java/src/test/T64.java
+++ java/src/test/T64.java
@@ -1,9 +1,10 @@
-import com.vitanuova.lib.Base64;
+import plan9.lib.Base64;
 
+// test base64 decoding
 public class T64 {
 	public static void main(String[] args){
-		Base64 encoding = new Base64();
-		String s = encoding.enc(args[0].getBytes());
+		Base64 base64 = new Base64();
+		String s = base64.enc(args[0].getBytes());
 		System.out.println(s.length()+": "+s);
 	}
 }
